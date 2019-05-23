54a5f4eba0d725536d52c190bdcc1547e0701674
André Karge <andre.karge@uni-weimar.de>
2019-05-23 10:17:42 +0200

    change path of csv

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ warnings.filterwarnings('ignore')
 
 if __name__ == "__main__":
     #corpus_path = "/mnt/media/Corpora/AndreKarge_2019-04-03_NumericFeaturesCallVSVarDeclStatement/corpus.csv"
-    corpus_path = "/mnt/media/foo.csv"
+    #corpus_path = "/mnt/media/foo.csv"
+    corpus_path = "/mnt/media/data.csv"
     #nfe.run(corpus_path)
     tfe.run(corpus_path)
